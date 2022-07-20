107baf9cd2691618ec3f059a81545116311f0bb5
ManishChaudhary-0 <manishc@email.sc.edu>
2022-07-20 12:06:56 -0400

    Test V1

---
+++ frontend/src/components/feedback/analysis.js
@@ -0,0 +1,379 @@
+import { useEffect, useState, useRef } from 'react';
+import React from 'react';
+
+import { Bar } from 'react-chartjs-2';
+import {
+    Box,
+    Button,
+    Card,
+    CardContent,
+    CardHeader,
+    Divider,
+    useTheme,
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableRow,
+    TableSortLabel,
+    InputLabel,
+    FormControl,
+    Select,
+    MenuItem,
+    Grid,
+    IconButton,
+    Typography,
+    ButtonGroup,
+    Paper,
+    TextField,
+} from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import { styled } from '@mui/material/styles';
+import { tableCellClasses } from '@mui/material/TableCell';
+import CheckBoxIcon from '@mui/icons-material/CheckBox';
+
+import { feedbackCall, matetrialCall, recommendationCall } from 'src/utils/apihelper';
+
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { ContentCutOutlined } from '@mui/icons-material';
+import { faHandHolding } from '@fortawesome/free-solid-svg-icons';
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import LinearProgress from '@mui/material/LinearProgress';
+import Autocomplete from '@mui/material/Autocomplete';
+
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
+
+import { LocalizationProvider } from '@mui/x-date-pickers-pro';
+import { DateRangePicker } from '@mui/x-date-pickers-pro/DateRangePicker';
+import { AdapterDateFns } from '@mui/x-date-pickers-pro/AdapterDateFns';
+
+import { engineAnalysisCall } from 'src/utils/apihelper';
+import PerfectScrollbar from "react-perfect-scrollbar";
+
+
+import { exceptionViewer } from "../../../Exception-Viewer-Widget-Datasheet";
+
+export const Analysis = (props) => {
+
+
+
+    const [matrixData, setMatrixData] = useState(exceptionViewer);
+
+    const [TotalMatrixData, setTotalMatrixData] = useState([]);
+
+    
+
+    const classStyle = useStyles();
+
+    const [loading, setLoading] = useState(false);
+    const themeButton = createTheme({
+        palette: {
+        primary: {
+            main:"#00CC00",
+        },
+        secondary: {
+            main: '#FF0000',
+        },
+    
+        },
+    });
+
+
+    const [startDate, setStartDate] = React.useState(new Date());
+    const [endDate, setEndDate] = React.useState(new Date());
+
+    const formatYmd = (date) => date.toISOString().slice(0, 10);
+
+    const [dateLoaded, setDateLoaded] = useState(false);
+
+     
+
+    useEffect(async () => {
+        if (dateLoaded == true){
+          loadChartData();
+        }
+
+    });
+
+    async function loadChartData() {
+        setLoading(true);
+        localStorage.setItem("Start-Date", formatYmd(startDate));
+        localStorage.setItem("End-Date", formatYmd(endDate));
+        console.log("!!!");
+        let data = await engineAnalysisCall();
+        TotalMatrixData(data);
+        setLoading(false);
+
+
+        
+    }
+
+
+
+
+    return (
+
+        <Card {...props} className={classStyle.card}>
+            <CardHeader
+              action={
+                  <FormControl style={{ width: 300 }}>                
+
+                      {/* <Autocomplete
+                        disablePortal
+                        id="combo-box-demo"
+                        //options={plannerMaterials}
+                       //value={materialID}
+                        sx={{ width: 300 }}
+                        //onChange={handler}
+                        renderInput={(params) => <TextField {...params} label="Select Material" />}
+                      /> */}
+
+                    <DesktopDatePicker
+                    label="Start Date"
+                    value={startDate}
+                    onChange={(newValue) => {
+                        setStartDate(newValue);
+                    }}
+                    minDate={new Date('2017-01-01')}
+                    renderInput={(params) => <TextField {...params} />}
+                    />
+
+                    <Typography padding="5%">
+                        {/* To */}
+
+                    </Typography>
+
+                    <DesktopDatePicker
+                    label="End Date"
+                    value={endDate}
+                    onChange={(newValue) => {
+                        setEndDate(newValue);
+                        setDateLoaded(true);
+
+                    }}
+                    minDate={new Date('2017-01-01')}
+                    renderInput={(params) => <TextField {...params} />}
+                    />
+
+
+                    {/* <LocalizationProvider
+                        dateAdapter={AdapterDateFns}
+                        localeText={{ start: 'Check-in', end: 'Check-out' }}
+                        >
+                        <DateRangePicker
+                           value={value}
+                            onChange={(newValue) => {
+                            setValue(newValue);
+                            }}
+                            renderInput={(startProps, endProps) => (
+                            <React.Fragment>
+                                <TextField {...startProps} />
+                                <Box sx={{ mx: 2 }}> to </Box>
+                                <TextField {...endProps} />
+                            </React.Fragment>
+                            )}
+                        />
+                    </LocalizationProvider> */}
+
+
+
+
+                  </FormControl>
+              }
+              title="Recommendation Feedback Analysis"
+            />
+
+            <Divider />
+
+
+
+
+
+
+            <CardContent display="flex">
+
+            {loading ? <LinearProgress /> : 
+
+                <Typography paragraph variant="subtitle1" gutterBottom component="div">
+
+                {/* {recommendationText} */}
+
+                {/* {dddd.advice} */}
+                </Typography>
+            }
+
+            {/* <Box
+                sx={{
+                height: 100,
+                position: 'relative',
+                }}
+
+                display="flex"
+                justifyContent="flex-end"
+                alignItems="flex-end"
+            >
+                <ButtonGroup disableElevation variant="contained"  sx={{ height: 60,fontSize:20, width:'20%'}}>
+
+
+                <Button sx={{width:'50%', fontSize:20, fontWeight:0}} theme={themeButton} 
+                //onClick={handleReject} 
+                //color={!reject ? "inherit" : "secondary"}
+                >Reject</Button>
+
+
+                <Button sx={{width:'50%', fontSize:20, fontWeight:0}} theme={themeButton} 
+                // onClick={handleAccept} 
+                //color={reject ? "inherit" : "primary"}
+                >Agree</Button>
+
+
+                </ButtonGroup>
+            </Box> */}
+
+
+            </CardContent>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            <PerfectScrollbar>
+                <Box sx={{ height: "600px", overflow: "scroll" }}>
+                <Table stickyHeader={true}>
+                    <TableHead>
+                    <TableRow>
+                        <StyledTableCell>PLANNER_ID</StyledTableCell>
+                        <StyledTableCell>Material</StyledTableCell>
+                        <StyledTableCell>Advice</StyledTableCell>
+                        <StyledTableCell>Accepted</StyledTableCell>
+                        <StyledTableCell>Reason</StyledTableCell>
+                        <StyledTableCell>Elasped</StyledTableCell>
+                        <StyledTableCell>Started</StyledTableCell>
+                        <StyledTableCell>Finished</StyledTableCell>
+                        <StyledTableCell>Transaction</StyledTableCell>
+                        <StyledTableCell>Problem</StyledTableCell>
+                    </TableRow>
+                    </TableHead>
+                    <TableBody>
+                    {/* {matrixData.map((order) => ( */}
+                    {TotalMatrixData.map((order) => (
+
+
+                        // <TableRow
+                        //     onClick={() => setSelectedRow(row)}
+                        //     key={row.name}
+                        //     sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                        // >
+
+                        <StyledTableRow
+                        hover
+                        // key={order.MaterialID}
+                        key={order.requests["planner"]}
+                        >
+                        <StyledTableCell>
+                            {/* {order.MaterialID} */}
+                            {order[1].material}
+                        </StyledTableCell>
+
+                        <StyledTableCell>
+                            {/* {order.MaterialID} */}
+                            {order[1].material_7}
+                        </StyledTableCell>
+                        <StyledTableCell>
+                            {/* {order.MaterialID} */}
+                            {order[1].material_9}
+                        </StyledTableCell>
+
+                        <StyledTableCell style={{ textAlign: "center" }}>
+                            {/* {order.ExceptionCount} */}
+                            {order[0].count}
+                        </StyledTableCell>
+                        <StyledTableCell style={{ textAlign: "center" }}>
+                            {/* {(order.Percentage*100).toString().slice(0,4) + "%"} */}
+                            {order[0].percentage.toString().slice(0, 4) + " %"}
+                        </StyledTableCell>
+                        <StyledTableCell>
+                            {/* {order.PartDescription} */}
+                            {order[1].mat_description}
+                        </StyledTableCell>
+
+                        <StyledTableCell>
+                            {/* {order.PartDescriptionEng} */}
+                            {order[1].mat_description_eng}
+                        </StyledTableCell>
+                        </StyledTableRow>
+                    ))}
+                    </TableBody>
+                </Table>
+                </Box>
+            </PerfectScrollbar>
+
+
+            <Divider />
+
+
+            <Box
+            sx={{
+                display: 'flex',
+                justifyContent: 'flex-end',
+                p: 2
+            }}
+            >
+
+            </Box>
+      </Card>
+
+
+    );
+
+};
+
+const useStyles = makeStyles({
+    card: {
+      border: "2px solid",
+      borderColor: "#3a86ff",
+      boxShadow: "0 19px 38px rgba(1,0.75,1,0.75), 0 15px 12px rgba(0,0,0,0.22)",
+      // boxShadow: "9px 18px #3a86ff",   // AABDFF   ---   F1EFFE --- 6F6F6F --- 0166B1
+      // borderColor: '#C4C4C4',
+      marginTop: 0,
+      marginBottom:100
+    }
+});
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      //  backgroundColor: theme.palette.common.black, // Change to hex code
+      backgroundColor: "#3a86ff", // Change to hex code
+      color: theme.palette.common.black,
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+    },
+  }));
+  
+  const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+      border: 0,
+    },
+  }));
---
+++ frontend/src/components/healthscore/TableHeader.js
@@ -0,0 +1,131 @@
+import React from 'react';
+import {
+    Box,
+    Button,
+    Card,
+    CardHeader,
+    Table,
+    TableBody,
+    TableCell,
+    Tooltip,
+    TableHead,
+    TableRow,
+    TableSortLabel,
+    Slider,
+  } from "@mui/material";
+
+  import { makeStyles } from "@mui/styles";
+  import { styled } from '@mui/material/styles';
+  import { tableCellClasses } from '@mui/material/TableCell';
+
+
+  export default function TableHeader(props) {
+
+    const { valueToOrderBy, orderDirection, handleRequestSort } = props;
+
+    const createSortHandler = (property) => (event) => {
+        handleRequestSort(event, property);
+    }
+
+
+    return (
+        <TableHead width="100%">
+            <TableRow>
+
+
+                <StyledTableCell key="material" style={{ textAlign: "left" }}>
+                    <TableSortLabel 
+                        active={valueToOrderBy === "material"}
+                        direction={valueToOrderBy === "material" ? orderDirection : 'asc'}
+                        onClick={createSortHandler("material")}
+                        >
+                            MATERIAL ID
+                        </TableSortLabel>
+                </StyledTableCell>
+                <StyledTableCell key="date" style={{ textAlign: "left" }}>
+                    <TableSortLabel 
+                        active={valueToOrderBy === "date"}
+                        direction={valueToOrderBy === "date" ? orderDirection : 'asc'}
+                        onClick={createSortHandler("date")}
+
+                        >
+                            Date
+                        </TableSortLabel>
+                </StyledTableCell>
+                <StyledTableCell key="description" style={{ textAlign: "left" }}>
+                    <TableSortLabel 
+                        active={valueToOrderBy === "description"}
+                        direction={valueToOrderBy === "description" ? orderDirection : 'asc'}
+                        onClick={createSortHandler("description")}
+
+                        >
+                            Material Description Eng
+                        </TableSortLabel>
+                </StyledTableCell>
+                <StyledTableCell key="health" style={{ textAlign: "center" }}>
+                    <TableSortLabel 
+                        active={valueToOrderBy === "health"}
+                        direction={valueToOrderBy === "health" ? orderDirection : 'asc'}
+                        onClick={createSortHandler("health")}
+
+                        >
+                            Health Status
+                        </TableSortLabel>
+                </StyledTableCell>
+                <StyledTableCell key="button" style={{ textAlign: "left", paddingLeft:"4%" }}>
+                    <TableSortLabel 
+                        active={valueToOrderBy === "button"}
+                        direction={valueToOrderBy === "button" ? orderDirection : 'asc'}
+                       // onClick={createSortHandler("button")}
+
+                        >
+                            Graphs
+                        </TableSortLabel>
+                </StyledTableCell>
+
+
+
+
+
+
+
+            </TableRow>
+        </TableHead>
+    )        
+    
+
+
+}
+
+
+const useStyles = makeStyles({
+    card: {
+      border: "2px solid",
+      borderColor: "#3a86ff",
+      boxShadow: "0 19px 38px rgba(1,0.75,1,0.75), 0 15px 12px rgba(0,0,0,0.22)",
+     // boxShadow: "9px 18px #3a86ff",   // AABDFF   ---   F1EFFE --- 6F6F6F --- 0166B1
+      // borderColor: '#C4C4C4',
+      marginTop:25,
+      marginBottom:35
+    }
+  });
+  
+  const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: "#3a86ff", // Change to hex code
+      color: theme.palette.common.black,
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+    },
+  }));
+  
+  const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+      border: 0,
+    },
+  }));
